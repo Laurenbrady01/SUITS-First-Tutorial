195069504e39a27bb9dfa43ffe97cb7045ee3d47
Laurenbrady01 <laurenbrady01@tamu.edu>
2020-09-17 17:44:27 -0500

    spawns cubes left click, spheres right click

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -10,7 +10,8 @@ public class CameraControl : MonoBehaviour
     public float cameraSpeed = 10;
     public float clampAngle = 80;
 
-    public GameObject ballPrefab;
+    public GameObject prefab1;
+    public GameObject prefab2;
 
     // FixedUpdate for the move
     void Update()
@@ -69,7 +70,7 @@ public class CameraControl : MonoBehaviour
 
             if (didWeHitSomething)
             {
-                GameObject tempBall = GameObject.Instantiate(ballPrefab);
+                GameObject tempBall = GameObject.Instantiate(prefab1);
                 Collider ballCollider = tempBall.GetComponent<Collider>();
 
                 tempBall.transform.position = hit.point + (hit.normal * ballCollider.bounds.extents.y);
@@ -81,6 +82,33 @@ public class CameraControl : MonoBehaviour
             
 
            
+        }
+
+        // Mouse control
+        if (Input.GetMouseButtonDown(1)) // Right click
+        {
+            // Add stuff here
+            UnityEngine.Debug.Log("I pressed mouse 1");
+
+            Ray testRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            bool didWeHitSomething = Physics.Raycast(testRay.origin, testRay.direction, out hit);
+
+            if (didWeHitSomething)
+            {
+                GameObject tempBall = GameObject.Instantiate(prefab2);
+                Collider ballCollider = tempBall.GetComponent<Collider>();
+
+                tempBall.transform.position = hit.point + (hit.normal * ballCollider.bounds.extents.y);
+                tempBall.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+            }
+            UnityEngine.Debug.DrawLine(testRay.origin, testRay.origin + testRay.direction * 10, Color.red, 2);
+
+
+
+
         }
     }
 }
